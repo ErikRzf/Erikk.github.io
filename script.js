6cba381dfc0fb2abe0ef0ffb460e181edb98e6bf
Kharisma Ahmad Pradana <43243514+Danasaleho02@users.noreply.github.com>
2019-10-25 11:13:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+
+  function validasi(form)  {
+    var username = document.getElementById("username").value;
+    var password1 = document.getElementById("password1").value;
+    var password2 = document.getElementById("password2").value;
+    if (username != "" && password1 != "" && passwrod2 != "") {
+        return (true);
+    } else {
+        alert('Anda harus mengisi data dengan lengkap !') ;
+        return (false);
+    }
+}
+function validasireg() {
+  var fname = document.getElementById("fname").value;
+  var lname = document.getElementById("lname").value;
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if (fname !="" && lname !="" && username != "" && password != "" ) {
+      return true;
+  } else {
+      alert('Anda harus mengisi data dengan lengkap !');
+  }
+}
+
+
 var slideIndex = 1;
 showDivs(slideIndex);
 
